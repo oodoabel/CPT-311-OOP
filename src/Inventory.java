65e27d07812cfd6c28883d8cd209d4dd3516103d
oodoabel <get2abeloodo@gmail.com>
2025-03-11 10:05:59 +0100

    Encapsulation

--- src/Inventory.java
+++ src/Inventory.java
@@ -7,4 +7,15 @@ public class Inventory {
     public Inventory(){
         items = new ArrayList<>();
     }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public void displayInventory(){
+        for (Item item : items ) {
+            System.out.println("Item: " + item.getName() + ", Quantity: " + item.getQuantity());
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        Inventory inventory = new Inventory();
+
+        Item item1 = new Item("Apple",  20);
+        Item item2 = new Item("Sword",  2);
+        
+        inventory.addItem(item1);
+        inventory.addItem(item2);
+
+        inventory.displayInventory();
     }
 }
